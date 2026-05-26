317fdc79de4c4d237d9c52f9fdf99e5d22b3b4a3
Jolie <267069884+lilie-lln@users.noreply.github.com>
2026-05-27 00:45:08 +0800

    Create solution.cpp
    2022'

---
+++ Problem/1381. Design a Stack With Increment Operation/solution.cpp
@@ -0,0 +1,34 @@
+class CustomStack {
+public:
+    
+    int max;
+    vector<int> stk;
+    
+    CustomStack(int maxSize) {
+        max=maxSize;
+    }
+    
+    void push(int x) {
+        if(stk.size()<max){
+            stk.push_back(x);
+        }
+    }
+    
+    int pop() {
+        if(stk.size()==0){
+            return -1;
+        }
+        int x=stk.back();
+        stk.pop_back();
+        return x;
+    }
+    
+    void increment(int k, int val) {
+        int x=k;
+        if(stk.size()<k)
+            x=stk.size();
+        for(int i=0;i<x;i++){
+            stk[i]+=val;
+        }
+    }
+};
