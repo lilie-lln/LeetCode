cefb75b6b95f39921e2837c786d979aa2dc394f9
Jolie <267069884+lilie-lln@users.noreply.github.com>
2026-05-27 00:20:09 +0800

    Create solution.cpp
    2023'

---
+++ Problem/70. Climbing Stairs/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int find(int n, vector<int> &dp)
+    {
+        if (n <= 1) return 1;
+        if (dp[n] != -1) return dp[n];
+        return dp[n]=find(n - 1, dp) + find(n - 2, dp);
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1); 
+        return find(n,dp);
+    }
+};
